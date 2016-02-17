b8200d9893cfd859ac10e768e1b7191c6b78e1da
Martin Towner <martintowner@gmail.com>
2016-02-17 20:26:58 +0800

    create autocello.py

---
+++ autocello.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+import os
+import selenium
+
+def start_parking():
+    cello_web = r'https://cellopark.com.au'
+    pass
+
+
+if __name__ == '__main__':
+    start_parking()
